82f803e08dbab4735ab06fad728cb0186ec25f24
alsoraimi <alsoraimi46@gmail.com>
2017-04-12 13:45:18 -0400

    add loop for factorial

--- lab4/Program.cs
+++ lab4/Program.cs
@@ -10,13 +10,18 @@ namespace lab4
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("please enter an integer thats greater than 0 but less than 10.");
+
+
             int Num = int.Parse(Console.ReadLine());              //name integers
             int result = Num;                                     //user input                      
 
-            for (int i = 0; i < Num; i++)
+            for (int i = 1; i < Num; i++)       // input loop
             {
-                
+                result = result * i;
+                Console.WriteLine("{0}*{1}", Num, i);
             }
+            Console.WriteLine(result);
 
 
 
